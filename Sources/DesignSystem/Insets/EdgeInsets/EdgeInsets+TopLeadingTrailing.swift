c7c218fabcdedfea7324458e4d3987931640113f
Michael Kampouris <mkampouris@gmail.com>
2025-07-30 02:13:26 -0400

    Create EdgeInsets+TopLeadingTrailing.swift

---
+++ Sources/DesignSystem/Insets/EdgeInsets/EdgeInsets+TopLeadingTrailing.swift
@@ -0,0 +1,71 @@
+//
+//  TopLeadingTrailing.swift
+//  DesignSystem
+//
+//  Created by Michael Kampouris on 7/30/25.
+//
+
+import SwiftUI
+
+public extension EdgeInsets {
+
+    /// Top, leading, and trailing padding of 4pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing4)
+    /// ```
+    static let topLeadingTrailing4 = EdgeInsets(top: .p1, leading: .p1, bottom: .p0, trailing: .p1)
+
+    /// Top, leading, and trailing padding of 8pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing8)
+    /// ```
+    static let topLeadingTrailing8 = EdgeInsets(top: .p2, leading: .p2, bottom: .p0, trailing: .p2)
+
+    /// Top, leading, and trailing padding of 12pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing12)
+    /// ```
+    static let topLeadingTrailing12 = EdgeInsets(top: .p3, leading: .p3, bottom: .p0, trailing: .p3)
+
+    /// Top, leading, and trailing padding of 16pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing16)
+    /// ```
+    static let topLeadingTrailing16 = EdgeInsets(top: .p4, leading: .p4, bottom: .p0, trailing: .p4)
+
+    /// Top, leading, and trailing padding of 20pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing20)
+    /// ```
+    static let topLeadingTrailing20 = EdgeInsets(top: .p5, leading: .p5, bottom: .p0, trailing: .p5)
+
+    /// Top, leading, and trailing padding of 24pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing24)
+    /// ```
+    static let topLeadingTrailing24 = EdgeInsets(top: .p6, leading: .p6, bottom: .p0, trailing: .p6)
+
+    /// Top, leading, and trailing padding of 28pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing28)
+    /// ```
+    static let topLeadingTrailing28 = EdgeInsets(top: .p7, leading: .p7, bottom: .p0, trailing: .p7)
+
+    /// Top, leading, and trailing padding of 32pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing32)
+    /// ```
+    static let topLeadingTrailing32 = EdgeInsets(top: .p8, leading: .p8, bottom: .p0, trailing: .p8)
+
+    /// Top, leading, and trailing padding of 36pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing36)
+    /// ```
+    static let topLeadingTrailing36 = EdgeInsets(top: .p9, leading: .p9, bottom: .p0, trailing: .p9)
+
+    /// Top, leading, and trailing padding of 40pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingTrailing40)
+    /// ```
+    static let topLeadingTrailing40 = EdgeInsets(top: .p10, leading: .p10, bottom: .p0, trailing: .p10)
+}
