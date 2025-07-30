59dbfb9ffb496aa2ee78fbc9072e8271770c9f54
Michael Kampouris <mkampouris@gmail.com>
2025-07-30 02:13:19 -0400

    Create EdgeInsets+TopLeadingBottom.swift

---
+++ Sources/DesignSystem/Insets/EdgeInsets/EdgeInsets+TopLeadingBottom.swift
@@ -0,0 +1,71 @@
+//
+//  TopLeadingBottom.swift
+//  DesignSystem
+//
+//  Created by Michael Kampouris on 7/30/25.
+//
+
+import SwiftUI
+
+public extension EdgeInsets {
+
+    /// Top, leading, and bottom padding of 4pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom4)
+    /// ```
+    static let topLeadingBottom4 = EdgeInsets(top: .p1, leading: .p1, bottom: .p1, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 8pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom8)
+    /// ```
+    static let topLeadingBottom8 = EdgeInsets(top: .p2, leading: .p2, bottom: .p2, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 12pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom12)
+    /// ```
+    static let topLeadingBottom12 = EdgeInsets(top: .p3, leading: .p3, bottom: .p3, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 16pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom16)
+    /// ```
+    static let topLeadingBottom16 = EdgeInsets(top: .p4, leading: .p4, bottom: .p4, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 20pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom20)
+    /// ```
+    static let topLeadingBottom20 = EdgeInsets(top: .p5, leading: .p5, bottom: .p5, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 24pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom24)
+    /// ```
+    static let topLeadingBottom24 = EdgeInsets(top: .p6, leading: .p6, bottom: .p6, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 28pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom28)
+    /// ```
+    static let topLeadingBottom28 = EdgeInsets(top: .p7, leading: .p7, bottom: .p7, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 32pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom32)
+    /// ```
+    static let topLeadingBottom32 = EdgeInsets(top: .p8, leading: .p8, bottom: .p8, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 36pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom36)
+    /// ```
+    static let topLeadingBottom36 = EdgeInsets(top: .p9, leading: .p9, bottom: .p9, trailing: .p0)
+
+    /// Top, leading, and bottom padding of 40pt.
+    /// ```swift
+    /// VStack { ... }.padding(.topLeadingBottom40)
+    /// ```
+    static let topLeadingBottom40 = EdgeInsets(top: .p10, leading: .p10, bottom: .p10, trailing: .p0)
+}
