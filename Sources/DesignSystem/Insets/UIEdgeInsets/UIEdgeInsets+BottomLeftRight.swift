fe57c3e5bb3ff56327024de6dc3b1c3b4fd1557d
Michael Kampouris <mkampouris@gmail.com>
2025-07-30 02:28:30 -0400

    Create UIEdgeInsets+BottomLeftRight.swift

---
+++ Sources/DesignSystem/Insets/UIEdgeInsets/UIEdgeInsets+BottomLeftRight.swift
@@ -0,0 +1,71 @@
+//
+//  BottomLeftRight.swift
+//  DesignSystem
+//
+//  Created by Michael Kampouris on 7/30/25.
+//
+
+import UIKit
+
+public extension UIEdgeInsets {
+
+    /// Bottom, left, and right padding of 4pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight4
+    /// ```
+    static let bottomLeftRight4 = UIEdgeInsets(top: .p0, left: .p1, bottom: .p1, right: .p1)
+
+    /// Bottom, left, and right padding of 8pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight8
+    /// ```
+    static let bottomLeftRight8 = UIEdgeInsets(top: .p0, left: .p2, bottom: .p2, right: .p2)
+
+    /// Bottom, left, and right padding of 12pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight12
+    /// ```
+    static let bottomLeftRight12 = UIEdgeInsets(top: .p0, left: .p3, bottom: .p3, right: .p3)
+
+    /// Bottom, left, and right padding of 16pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight16
+    /// ```
+    static let bottomLeftRight16 = UIEdgeInsets(top: .p0, left: .p4, bottom: .p4, right: .p4)
+
+    /// Bottom, left, and right padding of 20pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight20
+    /// ```
+    static let bottomLeftRight20 = UIEdgeInsets(top: .p0, left: .p5, bottom: .p5, right: .p5)
+
+    /// Bottom, left, and right padding of 24pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight24
+    /// ```
+    static let bottomLeftRight24 = UIEdgeInsets(top: .p0, left: .p6, bottom: .p6, right: .p6)
+
+    /// Bottom, left, and right padding of 28pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight28
+    /// ```
+    static let bottomLeftRight28 = UIEdgeInsets(top: .p0, left: .p7, bottom: .p7, right: .p7)
+
+    /// Bottom, left, and right padding of 32pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight32
+    /// ```
+    static let bottomLeftRight32 = UIEdgeInsets(top: .p0, left: .p8, bottom: .p8, right: .p8)
+
+    /// Bottom, left, and right padding of 36pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight36
+    /// ```
+    static let bottomLeftRight36 = UIEdgeInsets(top: .p0, left: .p9, bottom: .p9, right: .p9)
+
+    /// Bottom, left, and right padding of 40pt.
+    /// ```swift
+    /// view.layoutMargins = .bottomLeftRight40
+    /// ```
+    static let bottomLeftRight40 = UIEdgeInsets(top: .p0, left: .p10, bottom: .p10, right: .p10)
+}
